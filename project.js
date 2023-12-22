76317b44c927cf16e0a3bb603cac8c4ee7657ef4
erkanzzafer <zzafererkann@gmail.com>
2023-12-22 16:26:00 +0300

    javascript sayac

---
+++ project.js
@@ -0,0 +1,75 @@
+const startStopBtn=document.querySelector('#startStopBtn');
+const resetBtn=document.querySelector('#resetBtn');
+
+
+let timerInterVal=null;
+let timerStatus=false;
+
+
+let seconds=0;
+let minutes=0;
+let hours=0;
+
+let leadingSeconds=0;
+let leadingMinutes=0;
+let leadingHours=0;
+
+
+function stopWatch(){
+	seconds++
+	if(seconds/60===1){
+		seconds=0;
+		minutes++;
+		if(minutes/60===1){
+			minutes=0;
+			hours++;
+		}
+	}
+	if(seconds<10){
+		leadingSeconds="0"+seconds.toString();
+	}else{
+		leadingSeconds=seconds;
+	}
+
+	if(minutes<10){
+		leadingMinutes="0"+minutes.toString();
+	}else{
+		leadingMinutes=minutes;
+	}
+
+	if(hours<10){
+		leadingHours="0"+hours.toString();
+	}else{
+		leadingHours=hours;
+	}
+
+
+
+	let displayTimer=document.getElementById('timer').innerText=leadingHours+":"+leadingMinutes + ":" + leadingSeconds;
+}
+
+resetBtn.addEventListener("click",function(){
+	window.clearInterval(timerInterVal);
+	seconds=0;
+	minutes=0;
+	hours=0;
+
+	document.getElementById('timer').innerHTML="00:00:00";
+	document.getElementById('startStopBtn').innerHTML='<i class="fa-solid fa-play" id="play"></i>';
+		timerStatus=false;
+})
+
+//window.setInterval(stopWatch,1000);
+startStopBtn.addEventListener("click",function(){
+	if(!timerStatus){
+
+		timerInterVal=window.setInterval(stopWatch,1000);
+		document.getElementById('startStopBtn').innerHTML='<i class="fa-solid fa-pause" id="pause"></i>';
+		timerStatus=true;
+	}else{
+		window.clearInterval(timerInterVal);
+		document.getElementById('startStopBtn').innerHTML='<i class="fa-solid fa-play" id="play"></i>';
+		timerStatus=false;
+
+	}
+})
